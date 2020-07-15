34929351584371f07ca578e619a5929290741229
Thorben Louw <thorben@tsamma.com>
2020-07-15 18:58:02 +0100

    [TL] A (serial) CPU version of the stencil program with Gaussian Blur

---
+++ main/CpuStencil.cpp
@@ -0,0 +1,84 @@
+#include <cstdlib>
+#include <cxxopts.hpp>
+#include <lodepng.h>
+#include <algorithm>
+#include "StructuredGridUtils.hpp"
+#include <limits>
+#include <chrono>
+#include "StencilUtils.hpp"
+
+using namespace stencil;
+
+// in and out are 4 channels of float pixel data stored in chan, row, col order
+// These are the zero-padded arrays
+auto gaussian_blur(const float *in, float *out, const unsigned nx, const unsigned ny) {
+    for (auto y = 1u; y < ny - 1; y++) {
+        for (auto x = 1u; x < nx - 1; x++) {
+            const auto idx = y * nx + x;
+            const int up = -nx;
+            const int down = +nx;
+            constexpr int left = -1;
+            constexpr int right = 1;
+            const auto nw = idx + up + left;
+            const auto ne = idx + up + right;
+            const auto n = idx + up;
+            const auto w = idx + left;
+            const auto m = idx;
+            const auto e = idx + right;
+            const auto sw = idx + down + left;
+            const auto s = idx + down;
+            const auto se = idx + down + right;
+            out[m] =
+                    (2.f * (in[n] + in[s] + in[w] + in[e]) + 1.f * (in[nw] + in[ne] + in[sw] + in[se]) + 4.0f * in[m]) /
+                    16.f;
+        }
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    std::string inputFilename, outputFilename;
+    unsigned numIters = 1u;
+
+    cxxopts::Options options(argv[0], " - Runs a 3x3 Gaussian Blur stencil over an input image a number of times");
+    options.add_options()
+            ("n,num-iters", "Number of iterations", cxxopts::value<unsigned>(numIters)->default_value("1"))
+            ("i,image", "filename input image (must be a 4-channel PNG image)",
+             cxxopts::value<std::string>(inputFilename))
+            ("o,output", "filename output (blurred image)", cxxopts::value<std::string>(outputFilename));
+    auto opts = options.parse(argc, argv);
+
+    auto maybeImg = loadPng(inputFilename);
+    if (!maybeImg.has_value()) {
+        return EXIT_FAILURE;
+    }
+
+    cout << inputFilename << " is " << maybeImg->width << "x" << maybeImg->height << " pixels in size." << std::endl;
+
+    alignas(64) auto img = std::make_unique<float[]>((maybeImg->width + 2) * (maybeImg->height + 2) * NumChannels);
+    alignas(64) auto tmp_img = std::make_unique<float[]>((maybeImg->width + 2) * (maybeImg->height + 2) * NumChannels);
+    auto fImageDescr = toPaddedFloatImage(*maybeImg, img);
+
+    std::cout << "Running " << numIters << "(x2) iterations of stencil";
+    auto tic = std::chrono::high_resolution_clock::now();
+
+    for (auto iter = 0u; iter < numIters; iter++) {
+        for (auto c = 0u; c < NumChannels; c++) {
+            const auto offset = c * fImageDescr.width * fImageDescr.height;
+            gaussian_blur(&img[offset], &tmp_img[offset], fImageDescr.width, fImageDescr.height);
+            gaussian_blur(&tmp_img[offset], &img[offset], fImageDescr.width, fImageDescr.height);
+        }
+    }
+
+    auto toc = std::chrono::high_resolution_clock::now();
+    auto diff = std::chrono::duration_cast<std::chrono::duration<double>>(toc - tic).count();
+    std::cerr << " took " << std::right << std::setw(12) << std::setprecision(5) << diff << "s" << std::endl;
+
+    auto cImg = toUnpaddedCharsImage(fImageDescr, img);
+
+    if (!savePng(cImg, outputFilename)) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
---
+++ main/StencilUtils.hpp
@@ -0,0 +1,131 @@
+#ifndef LBM_GRAPHCORE_GRAPHCOREUTILS_H
+#define LBM_GRAPHCORE_GRAPHCOREUTILS_H
+namespace stencil {
+    constexpr auto NumChannels = 4u; // RGBA
+
+    typedef struct {
+        unsigned width;
+        unsigned height;
+        std::vector<unsigned char> bytes; // Note: this is in row, col, channel order
+    } Image;
+
+    typedef struct {
+        unsigned width;
+        unsigned height;
+        unsigned origChanMin[NumChannels]; // Note the original image's min and max values per chan so that we can reconstruct an image with the same brightness
+        unsigned origChanMax[NumChannels];
+    } ImageDescriptor;
+
+    auto loadPng(const std::string &filename) -> std::optional<Image> {
+        auto img = Image{};
+
+        unsigned error = lodepng::decode(img.bytes, img.width, img.height, filename);
+
+        if (error) {
+            std::cerr << "PNG decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+            return nullopt;
+        }
+
+        return img;
+    }
+
+    auto savePng(const Image &image, const std::string &filename) -> bool {
+        unsigned error = lodepng::encode(filename, image.bytes, image.width, image.height);
+
+        if (error) std::cerr << "PNG encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+        return error == 0;
+    }
+
+// buffer should be pre-allocated to the right size
+    auto toPaddedFloatImage(const Image &image, const std::unique_ptr<float[]> &buffer) -> ImageDescriptor {
+        auto fImg = ImageDescriptor{
+                .width= image.width + 2,
+                .height= image.height + 2,
+                .origChanMin = {0, 0, 0, 0},
+                .origChanMax = {0, 0, 0, 0},
+        };
+        unsigned char max[4] = {0, 0, 0, 0};
+        constexpr auto MaxUChar = std::numeric_limits<unsigned char>::max();
+        unsigned char min[4] = {MaxUChar, MaxUChar, MaxUChar, MaxUChar};
+        for (auto idx = 0u; idx < image.width * image.height; idx++) {
+            const auto rgba = &image.bytes[idx * NumChannels];
+            for (auto chan = 0u; chan < NumChannels; chan++) {
+                max[chan] = std::max(max[chan], rgba[chan]);
+                min[chan] = std::min(min[chan], rgba[chan]);
+            }
+        }
+        for (auto c = 0u; c < NumChannels; c++) {
+            fImg.origChanMin[c] = min[c];
+            fImg.origChanMax[c] = max[c];
+            // copy the bytes to the middle
+            for (auto y = 0u; y < image.height; y++) {
+                for (auto x = 0u; x < image.width; x++) {
+                    const auto cIdx = (y * image.width + x) * NumChannels + c;
+                    const auto fIdx = c * (fImg.width * fImg.height) + (fImg.width * (y + 1)) + (x + 1);
+                    buffer[fIdx] = (max[c] == min[c])
+                                   ? 0.f :
+                                   ((float) image.bytes[cIdx] - (float) min[c]) / ((float) max[c] - (float) min[c]);
+                }
+            }
+            //zero out the top and bottom
+            for (auto y = 1u; y < fImg.height - 1; y++) {
+                const auto leftIdx = c * (fImg.width * fImg.height) + (fImg.width * y);
+                const auto rightIdx = leftIdx + fImg.width - 1;
+                buffer[leftIdx] = 0.f;
+                buffer[rightIdx] = 0.f;
+            }
+            //zero out the left and right
+            for (auto x = 0u; x < fImg.width; x++) {
+                const auto topIdx = c * (fImg.width * fImg.height) + x;
+                const auto bottomIdx = c * (fImg.width * fImg.height) + (fImg.width * (fImg.height - 1)) + x;
+                buffer[topIdx] = 0.f;
+                buffer[bottomIdx] = 0.f;
+            }
+        }
+
+        return fImg;
+    }
+
+    auto toUnpaddedCharsImage(const ImageDescriptor &floatImage, const std::unique_ptr<float[]> &buffer) -> Image {
+        auto img = Image{
+                .width= floatImage.width - 2,
+                .height= floatImage.height - 2,
+                .bytes= std::vector<unsigned char>(NumChannels * (floatImage.height - 2) * (floatImage.width - 2), 0)
+        };
+
+        // Find the min and max vals of each channel
+        float max[4] = {0.f, 0.f, 0.f, 0.f};
+        constexpr auto MaxFloat = std::numeric_limits<float>::max();
+        float min[4] = {MaxFloat, MaxFloat, MaxFloat, MaxFloat};
+
+        for (auto c = 0u; c < NumChannels; c++) {
+            for (auto y = 1u; y < floatImage.height - 1; y++) {
+                for (auto x = 1u; x < floatImage.width - 1; x++) {
+                    const auto idx = c * (floatImage.height * floatImage.width) + y * floatImage.width + x;
+                    min[c] = std::min(min[c], buffer[idx]);
+                    max[c] = std::max(max[c], buffer[idx]);
+                }
+            }
+        }
+        for (auto c = 0u; c < NumChannels; c++) {
+            for (auto y = 0u; y < img.height; y++) {
+                for (auto x = 0u; x < img.width; x++) {
+                    const auto inIdx = c * (floatImage.height * floatImage.width) + (y + 1) * floatImage.width + (x + 1);
+                    const auto outIdx = (y * img.width + x) * NumChannels + c;
+
+                    auto rescaled = (max[c] == min[c]) ? 0.f :
+                                    (buffer[inIdx] + min[c]) /
+                                    (max[c] - min[c]); // Now it's in the range 0..1
+                    auto inOrigBrightness =
+                            (rescaled * (float) (floatImage.origChanMax[c] - floatImage.origChanMin[c])) +
+                            (float) floatImage.origChanMin[c];
+                    img.bytes[outIdx] = (unsigned char) inOrigBrightness;
+                }
+            }
+        }
+
+        return img;
+    }
+
+}
+#endif
